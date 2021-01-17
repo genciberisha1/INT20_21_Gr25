137ac6a59823fcf15533cfa7811e4e3c16778e83
etrordermaku1 <76623917+etrordermaku1@users.noreply.github.com>
2021-01-17 02:12:03 +0100

    Added Dark-Theme switcher.

---
+++ js/Dark_Mode.js
@@ -0,0 +1,14 @@
+function switchTheme() {
+  document.body.classList.toggle("dark-theme");
+}
+
+
+ var currentTime = new Date().getHours();
+  if (document.body) {
+      if (6<= currentTime   && currentTime <18) {
+        document.getElementById('btn').click();
+        }
+      
+      }
+      
+
