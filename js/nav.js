fec4b75e40c358195523d8e106693b7f1509e080
etrordermaku1 <76623917+etrordermaku1@users.noreply.github.com>
2021-01-21 12:54:56 +0100

    Updated nav.js

--- js/nav.js
+++ js/nav.js
@@ -6,10 +6,6 @@ $('.btn').click(function(){
       $('nav ul .feat-show').toggleClass("show");
       $('nav ul .first').toggleClass("rotate");
     });
-    $('.serv-btn').click(function(){
-      $('nav ul .serv-show').toggleClass("show1");
-      $('nav ul .second').toggleClass("rotate");
-    });
     $('nav ul li').click(function(){
       $(this).addClass("active").siblings().removeClass("active");
     });
